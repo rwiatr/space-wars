6e838d92c5449d1ff2f36497707ca9b59abb7816
Roman Wiatr <rwiatr@gmail.com>
2015-01-02 09:20:13 +0100

    dropped predecessors functionality and added tests

--- src/clj/graph/traversal.clj
+++ src/clj/graph/traversal.clj
@@ -2,25 +2,17 @@
   (:gen-class))
 
 (defn- breath-first-seq-impl
-  ([node neighbour-fn head-fn]
-   (cons (head-fn node nil)
-         (lazy-seq (breath-first-seq-impl node (neighbour-fn node) #{node} neighbour-fn head-fn))))
-  ([pred nodes ignored neighbour-fn head-fn]
-   (loop [nodes nodes]
+  ([node neighbour-fn]
+   (cons node (lazy-seq (breath-first-seq-impl (neighbour-fn node) #{node} neighbour-fn))))
+  ([queue ignored neighbour-fn]
+   (loop [nodes queue]
      (let [node (first nodes)]
        (cond (empty? nodes) nil ; EOF seq
              (contains? ignored node) (recur (rest nodes)) ; try next node
-             :else (cons (head-fn node pred)
-                         (lazy-seq (breath-first-seq-impl
-                                    pred
-                                    (concat (rest nodes) (neighbour-fn node))
-                                    (conj ignored node)
-                                    neighbour-fn
-                                    head-fn))))))))
+             :else (cons node (lazy-seq (breath-first-seq-impl
+                                         (concat (rest nodes) (neighbour-fn node))
+                                         (conj ignored node)
+                                         neighbour-fn))))))))
 
 (defn breath-first-seq
-  ([node neighbour-fn] (breath-first-seq node neighbour-fn :nodes-only))
-  ([node neighbour-fn option]
-   (case option
-     :predecessors (breath-first-seq-impl node neighbour-fn (fn [node pred] [node pred]))
-     :nodes-only (breath-first-seq-impl node neighbour-fn (fn [node pred] node)))))
+  ([node neighbour-fn] (breath-first-seq-impl node neighbour-fn)))
--- test/clj/graph/traversal_test.clj
+++ test/clj/graph/traversal_test.clj
@@ -5,16 +5,28 @@
 
 (deftest test.breath-first
   (testing "one element graph"
-    (is (= '(a) (breath-first-seq 'a {} :nodes-only)))
-    (is (= '([a nil]) (breath-first-seq 'a {} :predecessors))))
+    (is (= '(a) (breath-first-seq 'a {}))))
+  (testing "one element with loop graph"
+    (is (= '(a) (breath-first-seq 'a {'a '(a)}))))
   (testing "two element directed graph"
-    (is (= '(a b) (breath-first-seq 'a {'a '(b)} :nodes-only)))
-    (is (= '([a nil] [b a]) (breath-first-seq 'a {'a '(b)} :predecessors))))
+    (is (= '(a b) (breath-first-seq 'a {'a '(b)}))))
   (testing "two element undirected graph"
-    (is (= '(a b) (breath-first-seq 'a {'a '(b) 'b '(a)} :nodes-only)))
-    (is (= '([a nil] [b a]) (breath-first-seq 'a {'a '(b) 'b '(a)} :predecessors))))
+    (is (= '(a b) (breath-first-seq 'a {'a '(b) 'b '(a)}))))
   (testing "three element directed graph"
-    (is (= '(a b c) (breath-first-seq 'a {'a '(b) 'b '(c)} :nodes-only)))
-    (is (= '([a nil] [b a] [c b]) (breath-first-seq 'a {'a '(b) 'b '(c)} :predecessors)))
-    (is (= '(a b c) (breath-first-seq 'a {'a '(b c)} :nodes-only)))
-    (is (= '([a nil] [b a] [c a]) (breath-first-seq 'a {'a '(b c)} :predecessors)))))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b) 'b '(c)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b) 'b '(c) 'c '(a)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b c)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b c) 'c '(b)}))))
+  (testing "three element undirected graph"
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b)
+                                          'b '(a c)
+                                          'c '(b)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b c)
+                                          'b '(a c)
+                                          'c '(a b)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b c)
+                                          'b '(a)
+                                          'c '(a)})))
+    (is (= '(a b c) (breath-first-seq 'a {'a '(b c)
+                                          'b '(a c)
+                                          'c '(a b)})))))
