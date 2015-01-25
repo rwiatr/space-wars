9a6cf0233a21f1f11e08761d92fd80d24b57e340
Roman Wiatr <rwiatr@gmail.com>
2015-01-25 09:38:30 +0100

    mapgraph tests

--- test/clj/graph/mapgraph_test.clj
+++ test/clj/graph/mapgraph_test.clj
@@ -20,25 +20,47 @@
 
 (deftest test.g-connect
   (testing "connection adds nodes to graph"
-    (is (is (-> (graph) (g-connect :a :b) (g-contains? :a :b))))
-    (is (is (-> (graph) (g-connect :a :b) (g-connect :a :c) (g-contains? :a :b :c)))))
+    (is (-> (graph) (g-connect :a :b) (g-contains? :a :b)))
+    (is (-> (graph) (g-connect :a :b) (g-connect :a :c) (g-contains? :a :b :c))))
   (testing "connection adds one way connection to graph"
-    (is (is (-> (graph) (g-connect :a :b) (g-connected? :a :b))))
-    (is (is (not (-> (graph) (g-connect :a :b) (g-bi-connected? :a :b)))))
-    (is (is (not (-> (graph) (g-connect :a :b) (g-connected? :b :a)))))))
+    (is (-> (graph) (g-connect :a :b) (g-connected? :a :b)))
+    (is (not (-> (graph) (g-connect :a :b) (g-bi-connected? :a :b))))
+    (is (not (-> (graph) (g-connect :a :b) (g-connected? :b :a))))))
 
 (deftest test.g-connect-to-many
-    (testing "connect-to-many adds nodes to graph"
-    (is (is (-> (graph) (g-connect-to-many :a :b :c :d) (g-contains? :a :b :c :d))))
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-contains? :a :b :c :d :e)))))
+  (testing "connect-to-many adds nodes to graph"
+    (is (-> (graph) (g-connect-to-many :a :b :c :d) (g-contains? :a :b :c :d))))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-contains? :a :b :c :d :e)))
   (testing "connect-to-many adds one way connection to graph"
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :a :b))))
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :a :c))))
-    (is (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :b :c)))))
-    (is (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-bi-connected? :a :b)))))
-    (is (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-bi-connected? :a :c))))))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :a :b)))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :a :c)))
+    (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-connected? :b :c))))
+    (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-bi-connected? :a :b))))
+    (is (not (-> (graph) (g-connect-to-many :a :b :c) (g-bi-connected? :a :c)))))
   (testing "connect-to-many does not overide existing connections"
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :b))))
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :c))))
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :d))))
-    (is (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :e))))))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :b)))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :c)))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :d)))
+    (is (-> (graph) (g-connect-to-many :a :b :c) (g-connect-to-many :a :d :e) (g-connected? :a :e)))))
+
+(deftest test.g-bi-connect
+  (testing "connection adds nodes to graph"
+    (is (-> (graph) (g-bi-connect :a :b) (g-contains? :a :b)))
+    (is (-> (graph) (g-bi-connect :a :b :c :d) (g-contains? :a :b :c :d)))
+    (is (-> (graph) (g-bi-connect :a :b) (g-bi-connect :a :c) (g-contains? :a :b :c))))
+  (testing "bi-connect adds two way connection to graph"
+    (is (-> (graph) (g-bi-connect :a :b) (g-bi-connected? :a :b)))
+    (is (-> (graph) (g-bi-connect :a :b :c :d) (g-bi-connected? :a :b)))
+    (is (-> (graph) (g-bi-connect :a :b :c :d) (g-bi-connected? :c :d)))
+    (is (not (-> (graph) (g-bi-connect :a :b :c :d) (g-connected? :c :b))))))
+
+(deftest test.g-connect-all
+  (testing "connect-all adds nodes to graph"
+    (is (-> (graph) (g-connect-all :a :b) (g-contains? :a :b)))
+    (is (-> (graph) (g-connect-all :a :b :c :d) (g-contains? :a :b :c :d)))
+    (is (-> (graph) (g-connect-all :a :b) (g-connect-all :a :c) (g-contains? :a :b :c))))
+  (testing "connectes all nodes with two way connections"
+    (is (-> (graph) (g-connect-all :a :b) (g-bi-connected? :a :b)))
+    (is (-> (graph) (g-connect-all :a :b :c) (g-bi-connected? :a :b)))
+    (is (-> (graph) (g-connect-all :a :b :c) (g-bi-connected? :a :c)))
+    (is (-> (graph) (g-connect-all :a :b :c) (g-bi-connected? :b :c)))))
