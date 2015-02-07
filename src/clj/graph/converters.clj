5018c5a03daa9496fb820420699b75318ac9f58d
Roman Wiatr <rwiatr@gmail.com>
2015-02-07 22:50:24 +0100

    partial implementation of to-graph converter

--- src/clj/graph/converters.clj
+++ src/clj/graph/converters.clj
@@ -4,7 +4,8 @@
             [util.set_multimap :refer [mm-reverse mm-index mm-merge mm-to-map mm-fmap mm-kv-fmap mm-filter del mm-kv-filter]]
             [geom.point :refer [point]]
             [geom.triangle :refer [triangle points to-circumcircle-center]]
-            [geom.polygon :refer [polygon]]))
+            [geom.polygon :refer [polygon]]
+            [graph.mapgraph :refer [graph g-connect-to-many g-add-prop]]))
 
 (defn neighbours? [t1 t2]
   (not-empty (clojure.set/intersection (:edges t1) (:edges t2))))
@@ -47,6 +48,32 @@
        (mm-fmap #(get point->polygon-indx %))
        (mm-filter some?)))
 
+(defn- key-mem [key-fn]
+  (let [ks (atom {})]
+    (fn [v]
+      (swap! ks #(if (% v) % (assoc % v (key-fn v))))
+      (@ks v))))
+
+(defn- inc-key [prefix]
+  (let [cnt (atom 0)]
+    (fn [v]
+      (swap! cnt inc)
+      (keyword (str prefix "-" @cnt)))))
+
+(defn as-graph
+  ([point->point-mm point->polygon-m] (as-graph (key-mem (inc-key "node")) point->point-mm point->polygon-m))
+  ([node-fn point->point-mm point->polygon-m]
+   (let [g (atom (graph))
+         pts (keys point->point-mm)
+         point->node-m (into {} (map #(vector % (node-fn %)) pts))]
+     (doseq [p pts] (do (let [n (point->node-m p)
+                              nds (map point->node-m (point->point-mm p))
+                              gm (:geometry (point->polygon-m p))]
+                          (swap! g g-connect-to-many n nds)
+                          (swap! g g-add-prop n :geometry gm))))
+     @g)))
+
+
 (defn voronoi-polygons->neighbours
   "transforms bowyer-watson triangles into voronoi diagram polygons mapped into their neighbours"
   [bowyer-watson-triangles]
@@ -55,11 +82,3 @@
                       point->points-indx (point->points point->triangles-indx)
                       point->polygon-indx (point->polygon point->triangles-indx)]
                   (polygon->polygons point->points-indx point->polygon-indx))))
-
-(defn into-g
-  ([connect-fn g [n n-seq]] (apply connect-fn g n n-seq))
-  ([connect-fn g [n n-seq] & other]
-   (apply into-g connect-fn (into-g connect-fn g [n n-seq]) other)))
-
-(defn into-graph [connect-fn g mm-seq]
-  (apply into-g connect-fn g (first mm-seq) (rest mm-seq)))
